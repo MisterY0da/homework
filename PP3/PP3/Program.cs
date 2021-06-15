8500ca4ffc3d235e87554b737a5275548a265e9c
= <=>
2021-06-15 03:55:57 +0300

    update PP3 project

--- PP3/PP3/Program.cs
+++ PP3/PP3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 
@@ -6,81 +7,134 @@ namespace PP3
 {
     class Program
     {
+        static Semaphore sem = new Semaphore(1, 1);
+        static double eps = 0.0000001;
+
+        static double iterationsToCreateThreads = (1 / eps) / 2;
+
+        static double resultMult;
+        static double resultSerial;
+
+        static int allIterations = 0;
+
+        static int threadIndex = 0;
+
+        const int threadsCount = 2;
+
+        static double[] sumsArr = new double[threadsCount];
+
         static void Main(string[] args)
         {
-            Serial();
-            MultiThreaded();
-        }
-
-
-        public static void Serial()
-        {
-            double start = 0.0;
-
-            double end = 9.0;
-
-            double epsilon = 0.001;
-
-            double totalValue = 0;
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            CalculateIntegralRecursively(start, end, epsilon, ref totalValue);
-            stopwatch.Stop();
-
-            Console.WriteLine("serial: " + Math.Round(totalValue, 2));
-            TimeSpan ts = stopwatch.Elapsed;
-            Console.WriteLine("Serial time: " + ts.Milliseconds + "ms\n");
-        }
-
-        public static void MultiThreaded()
-        {
-            double start = 0.0;
-
-            double end = 9.0;
-
-            double epsilon = 0.001;
-
-            double part1 = 0;
-            double part2 = 0;
-            double part3 = 0;
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            Thread t1 = new Thread(state => CalculateIntegralRecursively(start, 3.00, epsilon, ref part1));
-            t1.Start();
-            Thread t2 = new Thread(state => CalculateIntegralRecursively(start + 3.00, 6.00, epsilon, ref part2));
-            t2.Start();
-            Thread t3 = new Thread(state => CalculateIntegralRecursively(start + 6.00, end, epsilon, ref part3));
-            t3.Start();            
-            
-            stopwatch.Stop();
-
-            Thread.Sleep(1);
-
-            double totalValue = part1 + part2 + part3;
-            
-
-            Console.WriteLine("multi: " + Math.Round(totalValue, 2));
-
-            TimeSpan ts = stopwatch.Elapsed;
-            Console.WriteLine("Multithreaded time: " + ts.Milliseconds + "ms\n");
-        }
-
-
-        static void CalculateIntegralRecursively(double start, double end, double epsilon, ref double sum)
-        {
-            if (start >= end)
+            for(int i = 0; i < sumsArr.Length; i++)
             {
-                sum += Parabola(end) * epsilon;
+                sumsArr[i] = 0;
+            }
+
+            GetSerialInfo();
+
+            GetMultInfo();
+
+            
+            Console.WriteLine("threads count: " + threadIndex);
+
+            for(int i = 0; i < sumsArr.Length; i++)
+            {
+                Console.WriteLine("thread #" + i + " local sum: " + sumsArr[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void GetSerialInfo()
+        {
+            Stopwatch watchSerial = new Stopwatch();
+            watchSerial.Start();
+            SerialIntegration(2, 3, ref resultSerial);
+            watchSerial.Stop();
+
+            Console.WriteLine("serial time: " + watchSerial.ElapsedTicks + " ticks");
+            Console.WriteLine("serial result: " + resultSerial + "\n");
+        }
+
+        static void GetMultInfo()
+        {
+            Stopwatch watchMult = new Stopwatch();
+            watchMult.Start();
+            MultIntegration(2, 3, ref sumsArr[0]);
+            watchMult.Stop();
+            Console.WriteLine("Multithreaded time: " + watchMult.ElapsedTicks + " ticks");
+
+
+
+            Thread.Sleep(1000);
+
+            for(int i = 0; i < sumsArr.Length; i++)
+            {
+                resultMult += sumsArr[i];
+            }
+
+            Console.WriteLine("multithreaded result: " + resultMult + "\n");
+        }
+
+        static void MultIntegration(double startPoint, double endPoint, ref double res)
+        {
+            //allIterations++;
+
+            double left = startPoint;
+            double right = endPoint;
+            double length = right - left;
+            double middle = left + length / 2;
+
+            
+
+            if (length < eps)
+            {
+                res += Parabola(middle) * length;
+            }
+
+            else if (threadIndex < threadsCount)
+            {
+                threadIndex += 2;
+                Thread leftThread = new Thread(state => MultIntegration(left, middle, ref sumsArr[threadIndex - 2]));
+                leftThread.Start();
+
+                Thread rightThread = new Thread(state => MultIntegration(middle, right, ref sumsArr[threadIndex - 1]));
+                rightThread.Start();
             }
 
             else
             {
-                sum += Parabola(start + epsilon) * (epsilon);
-                CalculateIntegralRecursively(start + epsilon, end, epsilon, ref sum);
+                MultIntegration(left, middle, ref res);
+                MultIntegration(middle, right, ref res);
+            }
+
+            
+        }
+
+        static void SerialIntegration(double startPoint, double endPoint, ref double res)
+        {
+            
+            double left = startPoint;
+            double right = endPoint;
+            double length = right - left;
+            double middle = left + length / 2;
+
+            
+
+            if (length < eps)
+            {
+                res += Parabola(middle) * length;
+            }
+
+            else
+            {
+                SerialIntegration(left, middle, ref res);
+
+                SerialIntegration(middle, right, ref res);
             }
         }
+
 
         static double Parabola(double x)
         {

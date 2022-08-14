94024cab5dd828e3110562de6f7fb21e8101864e
MisterY0da <pretty_good.guy@mail.ru>
2022-08-14 13:37:45 +0300

    add cleverencetask1

---
+++ CleverenceTask1/CleverenceTask1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace CleverenceTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int clientsCount = 30;
+            var rnd = new Random();
+
+            Thread[] clientsThreads = new Thread[clientsCount];
+
+            for(int i = 0; i < clientsCount; i++)
+            {
+                if(i % 5 == 0)
+                {
+                    clientsThreads[i] = new Thread(state => Server.AddToCount(rnd.Next(1, 10)));
+                }
+                else
+                {
+                    clientsThreads[i] = new Thread(state => Server.GetCount());
+                }
+            }
+
+            foreach(var th in clientsThreads)
+            {
+                th.Start();
+                Thread.Sleep(10);
+            }
+           
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ CleverenceTask1/CleverenceTask1/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CleverenceTask1
+{
+    public static class Server
+    {
+        static int count;
+        static object lockObject = new object();
+
+        public static void GetCount()
+        {
+            Console.WriteLine("Читатель прочитал count = " + count + "\t id потока: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static void AddToCount(int value)
+        {
+            lock (lockObject)
+            {
+                count += value;
+            }
+
+            Console.WriteLine("Писатель прибавил " + value + "\t\t id потока: " + Thread.CurrentThread.ManagedThreadId + ", count = "  + count);
+        }
+    }
+}

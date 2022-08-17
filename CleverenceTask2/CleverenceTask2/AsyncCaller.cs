a0eb4aa015bb811dbf19f22a310262f785f799e3
MisterY0da <pretty_good,guy@mail.ru>
2022-08-17 16:00:03 +0300

    add cleverenceTask2 project

---
+++ CleverenceTask2/CleverenceTask2/AsyncCaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CleverenceTask2
+{
+    public class AsyncCaller
+    {
+        private readonly EventHandler handler;
+
+        public AsyncCaller(EventHandler handler)
+        {
+            this.handler = handler;
+        }
+
+        public bool Invoke(int timeout, object sender, EventArgs e)
+        {
+            var task = Task.Factory.StartNew(() => handler.Invoke(sender, e));
+
+            return task.Wait(timeout);
+        }
+    }
+}
---
+++ CleverenceTask2/CleverenceTask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace CleverenceTask2
+{
+    class Program
+    {
+        private static void MyEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Handler started");
+            Thread.Sleep(2000);
+            Console.WriteLine("Handler ended");
+        }
+        static void Main(string[] args)
+        {
+            EventHandler h = new EventHandler(MyEventHandler);
+            AsyncCaller ac = new AsyncCaller(h);
+            if (ac.Invoke(5000, null, EventArgs.Empty))
+            {
+                Console.WriteLine("Completed successfully");
+            }
+            else
+            {
+                Console.WriteLine("Time is out!");
+            }
+        }
+    }
+}

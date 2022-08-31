61a6dc1a01dcacc25f016d332565d3b1cd99fcef
MisterY0da <pretty_good,guy@mail.ru>
2022-08-31 19:43:35 +0300

    add testPath project

---
+++ testPath/testPath/Program.cs
@@ -0,0 +1,27 @@
+﻿string relativePath = @"C:\temp\..\temp\.\.\text\..\test.txt";
+string absolutePath = @"C:\temp\test.txt";
+
+Console.WriteLine(GetAbsolutePath(relativePath));
+
+string GetAbsolutePath(string relativePath)
+{
+    List<string> relativeWords = relativePath.Split(@"\").ToList();
+    relativeWords.RemoveAll(e => e.Equals("."));
+
+    Stack<string> resultStack = new Stack<string>();
+
+    for (int i = 0; i < relativeWords.Count; i++)
+    {
+        if (relativeWords[i].Equals(".."))
+        {
+            resultStack.Pop();
+        }
+
+        else
+        {
+            resultStack.Push(relativeWords[i]);
+        }
+    }
+
+    return string.Join(@"\", resultStack.Reverse());
+}
